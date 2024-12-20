904ee3671440af281673bb9165d015888a1063ee
Matija <matijazvezda@gmail.com>
2024-12-20 23:18:13 +0100

    Fix ESLint unused variable issue

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -57,12 +57,11 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username || "default_username",
-      photo: image_url || "default_image_url",
+      username: username!,
+      photo: image_url!,
       firstName: first_name,
       lastName: last_name,
     };
-    
 
     console.log(user);
 
